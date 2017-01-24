4b8a6368072d0164d84018a9eb89630754b86ad7
Svegin <svegin@Svegins-Mac-mini.local>
2017-01-24 14:30:24 -0600

    one time click

--- master.js
+++ master.js
@@ -1,10 +1,21 @@
 const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
 
 
 function logText(e) {
-  console.log(this.classList.value);
-  e.stopPropagation();//stop bubbling
+    console.log(this.classList.value);
+    // e.stopPropagation();//stop bubbling
 }
 
 
-divs.forEach(div => div.addEventListener('click', logText));
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false,
+    once: true //click only one time
+}));
+
+
+button.addEventListener('click', () => {
+    console.log('click????');
+}, {
+    once: true // click only one time
+});
